3b1e479f39ea3f95b2758bfa5369b6386b764646
ADRPF <felipep.almeida15@gmail.com>
2023-08-23 13:25:40 -0400

    configured admin site

--- MPS/RU/admin.py
+++ MPS/RU/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+admin.site.register(CorpoAcad)
+admin.site.register(Admin)
+admin.site.register(Pedido)
+admin.site.register(Cardapio)
+admin.site.register(Prato)
+admin.site.register(Cadastro)
+#admin.site.register(Feedback)
