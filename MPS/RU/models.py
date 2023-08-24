5438cedce62ab8a4e5588038a9d72c762ac6720a
ADRPF <felipep.almeida15@gmail.com>
2023-08-23 20:30:57 -0400

    Added Materials to models and fixed relationship Cardapio-Pedido

--- MPS/RU/models.py
+++ MPS/RU/models.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here.
 
 SEXO_CHOICES = [
-        ("F", "FEMININO"),
-        ("M", "MASCULINO"),
-        ("N", "NENHUMA DAS OPCOES")
+        ('F', 'FEMININO'),
+        ('M', 'MASCULINO'),
+        ('N', 'NENHUMA DAS OPCOES')
 ]
 
 
@@ -22,7 +22,7 @@ class CorpoAcad(models.Model):
     matricula = models.CharField(max_length=10, blank=False, null=False, unique=True)
     nome = models.CharField(max_length=100, default='Sem Nome', blank=False, null=False)
     dtNascimento = models.DateField(auto_now=False)
-    sexo = models.CharField(max_length=1, choices=SEXO_CHOICES, blank=False, null=False)
+    sexo = models.CharField(max_length=1, choices=SEXO_CHOICES, default='N', blank=False, null=False)
     cadastro = models.ForeignKey(Cadastro, on_delete=models.CASCADE)
 
     def __str__(self):
@@ -47,17 +47,44 @@ class Pedido(models.Model):
                  (CREDITO, 'CREDITO')]
 
     horario = models.DateTimeField(auto_now=True)
-    formaPag = models.CharField(max_length=2, choices=FORMA_PAG, default=DINHEIRO)
+    formaPag = models.CharField(max_length=2, choices=FORMA_PAG, default=DINHEIRO, blank=False, null=False)
     corpoAcad = models.ForeignKey(CorpoAcad, on_delete=models.CASCADE)
+    prato = models.ManyToManyField('Prato')
 
     def __str__(self):
         return 'pedido: ' + str(self.pk)
+
+
+class Material(models.Model):
+    VERDURA = 'V'
+    FRUTA = 'F'
+    CARNE = 'C'
+    FRUTO_DO_MAR = 'M'
+    BEBIDA = 'B'
+    NENHUM = 'N'
+
+    TIPO_MATERIAL = [
+        (VERDURA, 'VERDURA'),
+        (FRUTA, 'FRUTA'),
+        (CARNE, 'CARNE'),
+        (FRUTO_DO_MAR, 'FRUTO_DO_MAR'),
+        (BEBIDA, 'BEBIDA'),
+        (NENHUM, 'NENHUM')
+    ]
+
+    tipoMaterial = models.CharField(max_length=1, default=NENHUM, blank=False, null=False)
+    material = models.CharField(max_length=50, choices=TIPO_MATERIAL, default=NENHUM, blank=False, null=False)
+    quantidade = models.IntegerField()
+
+    def __str__(self):
+        return self.material
 
 
 class Prato(models.Model):
     nome = models.CharField(max_length=100, default='Sem Nome', blank=False, null=False)
     valor = models.CharField(max_length=4, default='0.00', blank=False, null=False)
     desc = models.CharField(max_length=1000, default='Sem desc.', blank=False, null=False)
+    material = models.ManyToManyField(Material)
 
     def __str__(self):
         return self.nome
@@ -76,9 +103,8 @@ class Cardapio(models.Model):
         (JANTA, 'JANTA')
     ]
 
-    tipoRefeicao = models.CharField(max_length=2, choices=REFEICOES, default='NAO SELECIONADO')
-    horarioRefeicao = models.DateTimeField(auto_now=True)
-    pedido = models.ForeignKey(Pedido, on_delete=models.CASCADE)
+    tipoRefeicao = models.CharField(max_length=2, choices=REFEICOES, default='NAO SELECIONADO', blank=False, null=False)
+    diaRefeicao = models.DateField(auto_now=True)
     prato = models.ManyToManyField(Prato)
 
     def __str__(self):
